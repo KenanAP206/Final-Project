8b47db9d1075c0ce08e6b5d77ffdc11fb23d3a9d
Kenko <kerimlikenan00@gmail.com>
2025-02-13 22:50:51 +0400

    comment part done

--- MVC/View/src/Components/User/Movie/Comment/index.jsx
+++ MVC/View/src/Components/User/Movie/Comment/index.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router';
+import { DiscussionEmbed } from 'disqus-react';
+import { FaStar } from "react-icons/fa";
+import { IoEye } from "react-icons/io5";
+import { IoPlayOutline, IoClose } from "react-icons/io5";
+import './Comment.css';
 
 function index() {
+  const { id } = useParams();
+  const [show, setShow] = useState(null);
+
+
+
+
+ 
+  const disqusConfig = {
+    url: `http://localhost:3000/shows/${id}`, 
+    identifier: `show_${id}`, 
+    title: show?.name || 'Show Page' 
+  };
+
   return (
-    <div>
-      Comment
-    </div>
-  )
+    <section className='comments'>
+      <div className="comments-section" style={{ padding: '20px' }}>
+        <DiscussionEmbed
+          shortname='watchmaker'
+          config={disqusConfig}
+        />
+      </div>
+    </section>
+  );
 }
 
-export default index
+export default index;
